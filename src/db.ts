5004d6b4d7c6d239282c86d4d6260ef40cce83d6
Aimberê Rocha Mendonça <aimbererm@gmail.com>
2025-10-09 16:13:03 -0300

    refactor: schema.prisma

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,4 @@
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from "@/generated/prisma";
 
 export const db = new PrismaClient().$extends(withAccelerate());
