7814711044d18fa148a50257fa07628f64f2f366
Aimberê Rocha Mendonça <aimbererm@gmail.com>
2025-10-11 16:16:24 -0300

    add prisma accelerate

--- src/db-edge.ts
+++ src/db-edge.ts
@@ -1,3 +1,4 @@
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { PrismaClient } from "@prisma/client/edge";
 
-export const db = new PrismaClient();
+export const db = new PrismaClient().$extends(withAccelerate());
--- src/db.ts
+++ src/db.ts
@@ -1,3 +1,4 @@
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { PrismaClient } from "@prisma/client";
 
-export const db = new PrismaClient();
+export const db = new PrismaClient().$extends(withAccelerate());
