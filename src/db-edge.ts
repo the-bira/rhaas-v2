905a8f05766f881ce0d25d679e6bbe970fa9a523
Aimberê Rocha Mendonça <aimbererm@gmail.com>
2025-10-11 16:07:31 -0300

    fix: prisma accelerate config

--- src/db-edge.ts
+++ src/db-edge.ts
@@ -1,4 +1,3 @@
-import { withAccelerate } from "@prisma/extension-accelerate";
 import { PrismaClient } from "@prisma/client/edge";
 
-export const db = new PrismaClient().$extends(withAccelerate());
+export const db = new PrismaClient();
--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,3 @@
-import { withAccelerate } from "@prisma/extension-accelerate";
 import { PrismaClient } from "@prisma/client";
 
-export const db = new PrismaClient().$extends(withAccelerate());
+export const db = new PrismaClient();
